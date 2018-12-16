b81c61313d52a6c20e7a28dcfe8da10335c64d75
Dean Zhu <deanzhu1@gmail.com>
2018-12-17 00:31:20 +0100

    reafegit marge

--- src/main.cc
+++ src/main.cc
@@ -59,7 +59,7 @@ void TestShuffle(int shinglesize) {
   }
   MinHash M(Kshingles, 10);
   OnePermutationHash OPH(Kshingles, 3, 2);
-  LSHBanding LSH(Kshingles, 20);
+  LSHBanding LSH(Kshingles, 50);
   LSHOPBanding LSHOP(Kshingles, 4, 10);
 
   cout << "Test de Precision del Minhash" << endl;
